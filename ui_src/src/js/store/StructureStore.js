7ffa97dd72429a0f6fb5a8bf4e473328b0430087
David Kunzmann <davidkunzmann@gmail.com>
2019-11-19 09:00:13 +0100

    Corrected endpoint for structure

--- ui_src/src/js/store/StructureStore.js
+++ ui_src/src/js/store/StructureStore.js
@@ -129,11 +129,7 @@
     const init = function () {
         if (!datas && !structureStore.is("DATAS_LOADING")) {
             structureStore.toggleState("DATAS_LOADING", true);
-<<<<<<< HEAD
-            $.get("http://localhost/statistics/data", function (response) {
-=======
-            $.get("http://localhost/statistics/data/structure.json", function (response) {
->>>>>>> Corrected data path
+            $.get(window.location.protocol + "//" + window.location.host + "/statistics/data/structure.json", function (response) {
                 datas = response;
                 if (datas) {
                     loadData(datas);
