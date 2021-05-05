db3cfe412e73f88a72a7bb693fc3dd1cc625740e
Gilles Dénervaud <gilles.denervaud@gmail.com>
2021-05-05 15:32:11 +0200

    update license

--- ui_src/src/js/lib/md5.js
+++ ui_src/src/js/lib/md5.js
@@ -1,20 +1,24 @@
 /*
- * JavaScript MD5
- * https://github.com/blueimp/JavaScript-MD5
+ * Copyright 2018 - 2021 Swiss Federal Institute of Technology Lausanne (EPFL)
  *
- * Copyright 2011, Sebastian Tschan
- * https://blueimp.net
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Licensed under the MIT license:
- * https://opensource.org/licenses/MIT
+ * http://www.apache.org/licenses/LICENSE-2.0.
  *
- * Based on
- * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
- * Digest Algorithm, as defined in RFC 1321.
- * Version 2.2 Copyright (C) Paul Johnston 1999 - 2009
- * Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet
- * Distributed under the BSD License
- * See http://pajhome.org.uk/crypt/md5 for more info.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This open source software code was developed in part or in whole in the
+ * Human Brain Project, funded from the European Union's Horizon 2020
+ * Framework Programme for Research and Innovation under
+ * Specific Grant Agreements No. 720270, No. 785907, and No. 945539
+ * (Human Brain Project SGA1, SGA2 and SGA3).
+ *
  */
 
 /* global define */
