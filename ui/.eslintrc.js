97434a9ef227dde7e11d78a46783af3d943fa819
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2022-05-06 15:54:14 +0300

    fix linting

---
+++ ui/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-undef": 0,
+        "no-useless-escape": 0,
+        "no-fallthrough": 0,
+        "no-extra-semi": 0,
+        "no-unused-vars": 0,
+        "no-cond-assign": 0,
+        "no-prototype-builtins": 0,
+        "no-empty": 0,
+        "no-constant-condition": 0,
+        "no-extra-boolean-cast": 0,
+        "no-dupe-else-if": 0,
+        "no-control-regex": 0
+    }
+}
