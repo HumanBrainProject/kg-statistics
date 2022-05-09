c25709763479224210604133dd1c7f842c9a3fe4
Oliver Schmid <oliver.schmid@ebrains.eu>
2022-05-09 15:21:19 +0200

    security hotspot and removal of fixme

--- service/src/main/java/eu/ebrains/kg/statistics/ServiceApplication.java
+++ service/src/main/java/eu/ebrains/kg/statistics/ServiceApplication.java
@@ -18,13 +18,13 @@ public class ServiceApplication extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
+	@SuppressWarnings("java:S4502") //We suppress the csrf disable warning because we have a stateless, token-base API (also see https://www.baeldung.com/spring-security-csrf#stateless-spring-api ).
 	protected void configure(HttpSecurity http) throws Exception {
 		/**
 		 *  The http security is quite simple here because we're just fast-forwarding the token
 		 *  ( {@link eu.ebrains.kg.service.configuration.OauthClient ) to KG core and
 		 *  let this one manage the access permissions....
 		 */
-		//FIXME We want to enable CSRF -> https://kanban.ebrains.eu/kg/planning/-/issues/635
 		http.csrf().disable();
 		http.authorizeRequests(a -> a.anyRequest().permitAll());
 	}
